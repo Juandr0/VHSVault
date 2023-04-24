56d28b3a181634020b1f54fe0fea70be3fbb4a51
Alex <alexander.cg.carlsson@gmail.com>
2023-04-24 10:00:13 +0200

    added Routes navigation to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import OrderConfirmationView from './Views/OrderConfirmationView';
 import ShoppingCartView from './Views/ShoppingCartView';
 import Navbar from './Components/Navbar';
 import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 
 
@@ -12,10 +13,30 @@ function App() {
 
   return (
     <div>
-      <Navbar/>
-      <HomePageView/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+            <HomePageView />
+          }
+        />
+
+        <Route  path="/movie/id" element={
+          //Skapa funktionalitet för och lägg till filmens ID så att den syns i länken
+            <MovieInformationView />
+          } />
+
+        <Route path="/confirmation" element={
+          //Lägg till ordernr som ID? Fast då kan alla användare som skriver in ordernr
+          //se alla ordrar. Lägger på is. 
+          
+            <OrderConfirmationView />
+          } />
+
+        <Route path="/cart" element={
+            <ShoppingCartView />
+          } />
+      </Routes>
     </div>
-   
   )
 };
 
