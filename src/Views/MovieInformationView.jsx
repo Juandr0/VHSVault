4a0ad6f89781aab1bb5a43c220aabcef1a36b316
Alexander <alexander@MacBook-Pro-som-tillhor-Alexander.local>
2023-05-05 14:55:15 +0200

    movieInfoView now properly fetches moviedata by id when url is freshly loaded git status

--- src/Views/MovieInformationView.jsx
+++ src/Views/MovieInformationView.jsx
@@ -1,23 +1,42 @@
-import { useLocation } from 'react-router-dom';
+import { json, useLocation } from 'react-router-dom';
 import Movie from '../Model/Movie';
 import './CSS/MovieInformationView.css';
+import apiFetcher from '../Components/apiFetcher';
+import { useEffect, useState } from 'react';
 
 const MovieInformationView = () => {
     const location = useLocation();
-    const movie = location.state.movie;
-    const posterWidth = 500;
+    const [movie, setMovie] = useState(location.state?.movie);
 
-    return (
-        <div>
-            <Movie 
-                props = {movie}
-                withAddButton={true}
-                withDescription={true}
-                posterWidth={posterWidth}
-                runPriceAlgoritm={true}
-            />
-        </div>
-    )
+    useEffect(() => {
+        if (!movie) {
+            const providedMovieId = location.pathname.split('/')[2];
+            const fetchMovieData = async () => {
+                const movieData = await apiFetcher(null, null, providedMovieId);
+                    setMovie(movieData);   
+            }
+            fetchMovieData();       
+        }
+    }, [location.pathname]);
+
+
+    const posterWidth = 500;
+    console.log('moviedata \n\n\n\n\n' + movie);
+    if (movie?.title) {
+        return (
+            <div>
+                <Movie 
+                    props = {movie}
+                    withAddButton={true}
+                    withDescription={true}
+                    posterWidth={posterWidth}
+                    runPriceAlgoritm={true}
+                />
+            </div>
+        )
+    }
+
+ 
 }
 
 export default MovieInformationView;
